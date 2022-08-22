43746cf5f609207c16efec60d8c1bba3536350f6
Lautaro-Gonzalez <lgonzalez@aracargroup.com>
2022-08-21 21:47:46 -0300

    added rlz

--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -3,6 +3,11 @@ function Header () {
         <div >
           <header >
               <h1>EL 25 de Pregunta'</h1>
+              <h3>Reglas</h3>
+              <p># Una respuesta incorrecta da una proxima pregunta</p>
+              <p># Una respuesta correcta da una un punto y mas informacion</p>
+              <p># Si al final no tenes 25 puntos perdes</p>
+              <p># Si contestas todas bien, ganaste el premio 🎁</p>
           </header>
           <br/>
         </div>
